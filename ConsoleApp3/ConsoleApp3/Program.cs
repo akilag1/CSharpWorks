27a1a98e283eb993d687adfaf6c3b24831e2d170
KingJonAG <akilagodigamuwa@gmail.com>
2021-11-04 18:57:59 +0530

    consoleapp4

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static int highScore=50;
+        static string name="Amila";
+
+        static void Main(string[] args)
+        {
+            chkScore(10, "Anura");
+            Console.WriteLine("\n");
+            chkScore(100, "Kasuni");
+            Console.WriteLine("\n");
+            chkScore(80, "Aruni");
+        }
+
+        static void chkScore(int newScore, string newName)
+        {
+            if (newScore > highScore)
+            {
+                highScore = newScore;
+                name = newName;
+                Console.WriteLine("New HighScoren is " + newScore);
+                Console.WriteLine("New highscore holder is " + newName);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("The old highscore of " + highScore + " could not be broken and is still held by " + name);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Average Calculator");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Please Enter the score and press enter to submit. After done entering scores enter -1 to get the average.");
+            Console.WriteLine("-----------------------------");
+            int sum = 0;
+            int counter = 0;
+            while (true)
+            {
+                Console.Write("Score: ");
+                string scoreEnter = Console.ReadLine();
+                bool userEnter = int.TryParse(scoreEnter, out int score);
+                if (userEnter)
+                {
+                    if (score == -1)
+                    {
+                        float avearge = ((float)sum /counter);
+                        Console.WriteLine("Average: {0}",avearge);
+                        Console.ReadKey();
+                        break;
+                    }
+                    else if(score>=0 && score <= 20)
+                    {
+                        sum = sum + score;
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The numbers entered should only be between 0 and 20. Press enter to retry");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Only numbers are allowed. Press enter to try again");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
